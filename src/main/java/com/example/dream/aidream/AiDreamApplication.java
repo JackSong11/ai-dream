d4cf294d5c6c892054b71162b963ca128cc684a3
songzhiquan1 <songzhiquan1@jd.com>
2026-06-30 10:42:34 +0800

    init

---
+++ src/main/java/com/example/dream/aidream/AiDreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.dream.aidream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dream/aidream/AiDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dream.aidream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
