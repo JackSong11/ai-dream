de435b23ca1cbe4cf6b493947aff778bf20f24c2
songzhiquan1 <songzhiquan1@jd.com>
2026-06-30 17:32:36 +0800

    init 项目架构-先删除api，admin-web，admin-starter模块

--- dream-backend/dream-app-web/src/main/java/com/example/dream/app/web/controller/HelloController.java
+++ dream-backend/dream-app-web/src/main/java/com/example/dream/app/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.dream.app.web;
+package com.example.dream.app.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
